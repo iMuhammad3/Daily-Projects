5e4d488c6a524aa9f2c52ccda085661ce04ea6fe
Muhammad <auwalmuhd365@gmail.com>
2023-09-03 20:37:24 +0100

    added project

---
+++ September 2023/Notifications 3/script.js
@@ -0,0 +1,7 @@
+
+const bell = document.querySelector('.fa-bell')
+const container = document.querySelector('.container')
+
+bell.addEventListener('click', () => {
+    container.classList.toggle('hide')
+})
