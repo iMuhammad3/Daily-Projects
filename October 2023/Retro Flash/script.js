1df621002ae648c4fcdee7abad540093adac99f7
Muhammad <auwalmuhd365@gmail.com>
2023-10-05 20:13:27 +0100

    added project

---
+++ October 2023/Retro Flash/script.js
@@ -0,0 +1,6 @@
+// Add JavaScript code here
+
+setTimeout(() => {
+    document.querySelector('body > div').classList.remove('-translate-y-56')
+    document.querySelectorAll('body > div')[1].classList.remove('translate-y-56')
+}, 200)
