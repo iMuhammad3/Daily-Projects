0d2a6182aedd6dfe6199dc4f60cc731996bf70ab
Muhammad <auwalmuhd365@gmail.com>
2023-12-14 17:19:53 +0100

    add project

--- December 2023/iCodeMas #05/script.js
+++
@@ -1,8 +0,0 @@
-// Add JavaScript code here
-document.getElementById("closeBtn").addEventListener('click', (e) => {
-    const banner = e.target.parentElement
-    banner.classList.add("translate-x-full")
-    setTimeout(() => {
-        banner.remove()
-    }, 510)
-})
